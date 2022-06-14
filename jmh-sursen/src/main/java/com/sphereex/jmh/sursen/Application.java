fe29b7ed96f5d8c724c88f2790cbd59c30701de8
shardingsphere <dev@shardingsphere.apache.org>
2022-06-14 19:57:23 +0800

    Refactor : update the test data

--- jmh-sursen/src/main/java/com/sphereex/jmh/sursen/Application.java
+++ jmh-sursen/src/main/java/com/sphereex/jmh/sursen/Application.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class Application {
 
     public static void main(String... args) throws SQLException, IOException {
-        executeCommand();
+//        executeCommand();
         prepareData();
     }
 
--- jmh-sursen/src/main/resources/prepare.sql
+++ jmh-sursen/src/main/resources/prepare.sql
@@ -1,8 +1,7 @@
 update tb_f_user10 set name='TEST_USER_10' limit 100;
 update tb_f_user_cert10 set cert_type = 'IDENTITY_CARD', expire_date='2033-12-22' limit 100;
-#update tb_f_user_contact10 set area_code = '86',phone='15699999999' where phone like '156%' limit 100;
-update tb_f_user10 set id="XXXX-XXXX-XXXX" order by id limit 10;
-update tb_f_user_cert10 set user_id="XXXX-XXXX-XXXX" order by id limit 10;
-update tb_f_user_contact10 set user_id="XXXX-XXXX-XXXX" order by id limit 10;
-update tb_f_user_cert10 set cert_no="111111111111111111" order by id limit 10;
-update tb_f_user_contact10 set phone="19999999999" order by id limit 10;
+update tb_f_user10 set id="XXXX-XXXX-XXXX" order by id limit 1;
+update tb_f_user_cert10 set user_id="XXXX-XXXX-XXXX" order by id limit 1;
+update tb_f_user_contact10 set user_id="XXXX-XXXX-XXXX" order by id limit 1;
+update tb_f_user_cert10 set cert_no="111111111111111111" order by id limit 1;
+update tb_f_user_contact10 set phone="19999999999" order by id limit 1;
