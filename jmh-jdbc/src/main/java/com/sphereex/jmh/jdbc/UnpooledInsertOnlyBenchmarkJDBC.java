6f721c9a260baec31bc8387821c6900c6cb36511
wsm12138 <1473602179@qq.com>
2022-09-22 16:55:09 +0800

    add insert type for jmh-jdbc & remove can't mvn class & update pom

---
+++ jmh-jdbc/src/main/java/com/sphereex/jmh/jdbc/UnpooledInsertOnlyBenchmarkJDBC.java
@@ -0,0 +1,9 @@
+package com.sphereex.jmh.jdbc;
+
+import com.sphereex.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledInsertOnlyBenchmarkJDBC extends UnpooledInsertOnlyBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
+
+
+
