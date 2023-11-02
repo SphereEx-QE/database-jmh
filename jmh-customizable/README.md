7e4855896e6c4c445165725d1e66a06a58fe1f36
孙念君 Nianjun Sun <sunnianjun@apache.org>
2023-11-02 14:37:06 +0800

    Add : add README file for customizable

---
+++ jmh-customizable/README.md
@@ -0,0 +1,10 @@
+## JMH Customizable
+该模块可根据传入的 SQL 文件为测试用例，动态更新测试内容。
+
+### 构建
+进入项目根目录，执行构建命令：
+```
+./mvnw clean install -DskipTests
+```
+
+### 使用
--- jmh-customizable/src/main/java/com/sphereex/jmh/customizable/CustomizableBenchmark.java
+++ jmh-customizable/src/main/java/com/sphereex/jmh/customizable/CustomizableBenchmark.java
@@ -1,16 +1,21 @@
 package com.sphereex.jmh.customizable;
 
+import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -31,10 +36,14 @@ public class CustomizableBenchmark {
     
     private final List<String> sqlList = new ArrayList<>();
     
+    public static void main(String[] args) throws IOException {
+        Main.main(new String[]{CustomizableBenchmark.class.getName()});
+    }
+    
     @Setup(Level.Trial)
     public void setup() throws IOException, ClassNotFoundException, SQLException {
-        File configFile = new File(System.getProperty("conf"));
-        File sqlFile = new File(System.getProperty("script"));
+        File configFile = new File(System.getProperty("shardingsphere.configurationFile"));
+        File sqlFile = new File(System.getProperty("shardingsphere.script"));
         if (!configFile.exists()) {
             throw new FileNotFoundException("can not find the config file " + System.getProperty("conf"));
         }
@@ -62,6 +71,7 @@ public class CustomizableBenchmark {
     @Benchmark
     public void executeSQL() throws SQLException {
         for (String sql : sqlList) {
+            System.out.println("execute sql: " + sql);
             statement.execute(sql);
         }
     }
